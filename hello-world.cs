c1c283cd1a4a981fd504ee6b849c5cb0bdb32ed1
Antonio <agonzalez@gonzaleztek.com>
2017-05-10 14:59:02 -0600

    added loop reading / writing numbers to screen

--- hello-world.cs
+++ hello-world.cs
@@ -7,6 +7,13 @@ namespace hello.world
 		public static void Main(string[] arguments)
 		{
 			Console.WriteLine("Hello World!");
+			
+			int[] numbers = {4, 7, 19, 23, 28, 31};
+			
+			foreach (int number in numbers)
+			{
+				Console.WriteLine(number.ToString());
+			}
 		}
 	}
 }
